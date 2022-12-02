19bcbd0b9126b0d0786cbe9b4e4dccb5777fcd48
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-12-01 19:43:27 -0500

    Day 01

---
+++ day_01/part_01/run_it.main.kts
@@ -0,0 +1,36 @@
+import java.io.File
+
+fun loadInput(): String {
+    return File("./input.txt").readText()
+}
+
+fun parseInput(input: String): List<String> {
+    return input.split("\n")
+}
+
+fun main() {
+    val input = parseInput(loadInput())
+
+    val converted = input.map { if (it.isBlank()) null else it.toInt(10) }
+
+    val results = mutableListOf<Int>()
+    var currentSum = 0
+    converted.forEach {
+        when(it) {
+            null -> {
+                results.add(currentSum)
+                currentSum = 0
+            }
+            else -> currentSum += it
+        }
+    }
+    if (converted.last() != null) {
+        results.add(currentSum)
+    }
+
+    val maxVal = results.maxOrNull()
+    println(maxVal)
+}
+
+main()
+
